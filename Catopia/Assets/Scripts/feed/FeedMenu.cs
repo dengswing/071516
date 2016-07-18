465ca770adecc46212eeead02cd3d722a250be21
dengshiwei <dengsw@shinezone.com>
2016-07-18 19:13:01 +0800

    first project

---
+++ Catopia/Assets/Scripts/feed/FeedMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FeedMenu : MonoBehaviour
+{
+    public GameObject btnPrefab;
+    private string[] btnName = new string[] { "A", "B", "C" };
+    public RectTransform _content;
+
+    void Awake()
+    {
+        CreateMenu();
+    }
+
+    void Start()
+    {
+
+    }
+
+    void CreateMenu()
+    {
+        FeedButton btn;
+        foreach (var item in btnName)
+        {
+            btn = GameTools.AddChild(_content, btnPrefab).GetComponent<FeedButton>();
+            btn.BtnTitle = item;
+        }
+    }
+}
