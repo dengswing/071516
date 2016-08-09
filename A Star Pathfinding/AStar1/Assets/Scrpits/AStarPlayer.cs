fcc4a13ba289aa5025788eb2b627990a8094b545
dengshiwei <dengsw@shinezone.com>
2016-08-09 09:28:27 +0800

    demo

---
+++ A Star Pathfinding/AStar1/Assets/Scrpits/AStarPlayer.cs
@@ -0,0 +1,198 @@
+﻿using Pathfinding;
+using System;
+using UnityEngine;
+
+public class AStarPlayer : MonoBehaviour
+{
+    //移动速度;
+    float playerMoveSpeed = 1f;
+    float turningSpeed = 5;
+
+    //目标位置;
+    Vector3 targetPosition = Vector3.zero;
+
+    Seeker seeker;
+
+    //计算出来的路线;
+    Path path;
+
+    //当前点
+    int currentWayPoint = 0;
+
+    bool stopMove = true;
+
+    //Player中心点;
+    float playerCenterY = 1.0f;
+
+    Animator animator;
+
+    Action moveFinish;
+
+    Action jumpBack;
+
+    public float PlayerMoveSpeed
+    {
+        get { return playerMoveSpeed; }
+        set { playerMoveSpeed = value; }
+    }
+
+    public float TurningSpeed
+    {
+        get { return turningSpeed; }
+        set { turningSpeed = value; }
+    }
+
+    // Use this for initialization
+    void Awake()
+    {
+        seeker = GetComponent<Seeker>();
+        playerCenterY = transform.localPosition.y;
+        animator = GetComponent<Animator>();
+
+       // UIManager.Instance.AddUIEvent(UIHandler);
+    }
+
+    public void Jump(bool isJump = true)
+    {
+        animator.SetBool("IsJump", isJump);
+    }
+
+    public void PlayerEquipment()
+    {
+        animator.SetBool("IsPlayer", true);
+    }
+
+    public void StopEquipment()
+    {
+        animator.SetBool("IsPlayer", false);
+    }
+
+    public void StopMove()
+    {
+        animator.SetBool("IsWalk", false);
+    }
+
+    public void MoveTarget(Vector3 position, Action callback)
+    {
+        if (!targetPosition.Equals(Vector3.zero))
+        {
+            Debug.Log("move");
+            return;
+        }
+        moveFinish = callback;
+        targetPosition = position;
+        seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+    }
+
+    public void RotationTarget(Vector3 rotation, Action callback)
+    {
+      //  CatGameTools.UIGameObjectRotationBack(transform, rotation, 0.5f, callback);
+    }
+
+    public void PositionTarget(Vector3 position, float duration, Action callback)
+    {
+      //  CatGameTools.UIGameObjectPositionBack(transform, position, duration, callback);
+    }
+
+    private void UIHandler(bool isShow)
+    {
+        if (isShow)
+            animator.speed = 0;
+        else
+            animator.speed = 1;
+    }
+
+    //寻路结束;
+    private void OnPathComplete(Path p)
+    {
+        //Debug.Log("OnPathComplete = " + p.error);
+        if (!p.error)
+        {
+            currentWayPoint = 0;
+            path = p;
+            stopMove = false;
+            StartMove();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (path == null || stopMove)
+        {
+            return;
+        }
+
+        //根据Player当前位置和 下一个寻路点的位置，计算方向;
+        Vector3 currentWayPointV = new Vector3(path.vectorPath[currentWayPoint].x, path.vectorPath[currentWayPoint].y, path.vectorPath[currentWayPoint].z);
+
+        // Debug.Log(path.vectorPath[currentWayPoint].y);
+        // Vector3 currentWayPointV = new Vector3(path.vectorPath[currentWayPoint].x, playerCenterY, path.vectorPath[currentWayPoint].z);
+        Vector3 dir = (currentWayPointV - transform.position).normalized;
+        RotateTowards(dir);
+
+        //计算这一帧要朝着 dir方向 移动多少距离;
+        dir *= PlayerMoveSpeed * Time.fixedDeltaTime;
+
+        //计算加上这一帧的位移，是不是会超过下一个节点;
+        float offset = Vector3.Distance(transform.localPosition, currentWayPointV);
+
+        if (offset < 0.1f)
+        {
+            transform.localPosition = currentWayPointV;
+            currentWayPoint++;
+
+            if (currentWayPoint == path.vectorPath.Count)
+            {
+                stopMove = true;
+                currentWayPoint = 0;
+                path = null;
+                EndMove();
+            }
+        }
+        else
+        {
+            if (dir.magnitude > offset)
+            {
+                Vector3 tmpV3 = dir * (offset / dir.magnitude);
+                dir = tmpV3;
+
+                currentWayPoint++;
+
+                if (currentWayPoint == path.vectorPath.Count)
+                {
+                    stopMove = true;
+
+                    currentWayPoint = 0;
+                    path = null;
+                }
+            }
+            transform.localPosition += dir;
+        }
+    }
+
+    private void EndMove()
+    {
+        targetPosition = Vector3.zero;
+        if (moveFinish != null) moveFinish();
+    }
+
+    private void StartMove()
+    {
+       // animator.SetBool("IsWalk", true);
+    }
+
+    private void RotateTowards(Vector3 dir)
+    {
+        if (dir == Vector3.zero) return;
+
+        Quaternion rot = transform.rotation;
+        Quaternion toTarget = Quaternion.LookRotation(dir);
+
+        rot = Quaternion.Slerp(rot, toTarget, TurningSpeed * Time.deltaTime);
+        Vector3 euler = rot.eulerAngles;
+        euler.z = 0;
+        euler.x = 0;
+        rot = Quaternion.Euler(euler);
+        transform.rotation = rot;
+    }
+}
---
+++ A Star Pathfinding/AStar1/Assets/Scrpits/CatAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CatAI : MonoBehaviour
+{
+    //移动速度;
+    public float playerMoveSpeed = 1f;
+    public float turningSpeed = 5;
+    public List<GameObject> moveTargetList;
+    public bool mouseClickTest = false;
+    public float jumpTime = 0.5f;
+    public float resetTime = 5f;
+
+    private AStarPlayer player;
+    private int index;
+    private Vector3 initPosition;
+    private float iTime;
+    private bool isWait;
+
+    void Start()
+    {
+        player = GetComponent<AStarPlayer>();
+        player.TurningSpeed = turningSpeed;
+        player.PlayerMoveSpeed = playerMoveSpeed;  
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && mouseClickTest)
+        {
+            RaycastHit hit;
+            if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                return;
+            }
+            if (!hit.transform)
+            {
+                return;
+            }
+            player.MoveTarget(hit.point, null);
+        }
+    }
+}
